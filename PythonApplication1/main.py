4dfdce713e312c3377464cb0fc6894bd8f768103
knw <knw@NT900X3M>
2018-01-28 16:09:34 +0900

    Python for .NET 예제 추가

--- PythonApplication1/main.py
+++ PythonApplication1/main.py
@@ -2,17 +2,19 @@
 import sys
 #from panda_test_mod import *
 from numpy_mod import *
+from pythonnet_mod import *
 
 def hello_py():
     print('Hello python!!!!')
 
 def main():
     #hello_py()
+    print('This project was uploaed to gitbub!!!')
     #numpy_test()
     #dat=loadtxt_test()
     #panda_test()
-    loadtxt_test()
-    print('This project was uploaed to gitbub!!!')
+    #loadtxt_test()
+    pythonnet_test_str()
 
 if __name__ == "__main__":
     sys.exit(int(main() or 0))
---
+++ PythonApplication1/pythonnet_mod.py
@@ -0,0 +1,23 @@
+"""
+Python for .NET을 시험해 보기위한 파일임
+
+다음 사이트에 설치 및 사용방법이 있음
+
+http://pythonnet.github.io/
+
+* 사용을 하기 위해서는 .NET 4.0+ 이상의 CLR을 이용하여야 한다.
+* 위의 사이트에서 알려주는 파이썬 패키지 저장소로 가서
+  현재 사용중인 파이썬의 버전에 적합한 whl파일을 다운로드한다. 
+
+* 먼저 "Anaconda prompt"를 관리자 권한으로 실행시켜야한다.
+* 실행된 관리자 CMD 창에서 다음 명령으로 설치한다.
+
+* pip install pythonnet~~~~.whl
+
+"""
+import clr
+
+def pythonnet_test_str():
+    clr.AddReference("System.Windows.Forms")
+    #from System.Windows.Forms import *
+
